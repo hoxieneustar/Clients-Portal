5f2745d4e93c6afabd7e54112dc6edd8fa6f1148
HOXIE x NEUSTAR <hoxieneustar@gmail.com>
2026-09-22 00:54:01 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -5,7 +5,7 @@
 
 const CONFIG_APP = {
     // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
-    namaVendor: "HOXIE PRODUCTION",
+    namaVendor: "HOXIE",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyCi15XA1h7eZz6IbT-qQEVKyJyaljLrFPU",
